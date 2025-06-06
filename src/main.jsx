fae17ec1fbeff2e541091790b5d2b91b403b45f4
bhumikadangayach <dangayach.bhumika18@gmail.com>
2025-06-06 14:46:37 +0530

    some more changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App2 from "./components/App2";
 import App3 from "./components/App3";
 import ReactDOM from "react-dom/client";
 import "./App.css";
+import "./index.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
